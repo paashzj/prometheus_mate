2dae0dddbdd90d12d741fb39bafa479d71ea123b
lovehzj <1922919664@qq.com>
2022-01-14 07:24:01 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # make directory
-mkdir $PROM_HOME/logs
-mkdir $PROM_HOME/mate/storage
-mkdir $PROM_HOME/mate/storage/jobs
+mkdir -p $PROM_HOME/logs
+mkdir -p $PROM_HOME/mate/storage
+mkdir -p $PROM_HOME/mate/storage/jobs
 
 nohup $PROM_HOME/mate/prom_mate >>$PROM_HOME/logs/prom_mate.stdout.log 2>>$PROM_HOME/logs/prom_mate.stderr.log &
 
